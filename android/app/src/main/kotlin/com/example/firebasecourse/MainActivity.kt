45e215443ec8032ba43f326e78a48431e8e7660f
Bishoy <bishoyabdo236@gmail.com>
2023-11-06 03:53:35 +0200

    Start with the UI

---
+++ android/app/src/main/kotlin/com/example/firebasecourse/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebasecourse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'package:firebasecourse/auth/login.dart';
+import 'package:firebasecourse/auth/signup.dart';
+import 'package:firebasecourse/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+      routes: {
+        "signup" : (context) => SignUp() ,
+        "login" : (context) => Login()
+      },
+    );
+  }
+}
